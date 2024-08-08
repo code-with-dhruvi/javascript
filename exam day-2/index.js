593153aad8b06c923dd9ed23a72139cb88f64965
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-08-08 22:31:16 +0530

    javascript

---
+++ exam day-2/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const task = document.getElementById('task');
+    const add = document.getElementById('add');
+    const List = document.getElementById('List');
+});
