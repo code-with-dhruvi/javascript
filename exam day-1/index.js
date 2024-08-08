a1c37db8f090709391ee4098329e061149251128
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-08-08 12:16:11 +0530

    javascript

--- exam day-1/index.js
+++ exam day-1/index.js
@@ -1,5 +1,26 @@
 let users=[];
-const handaledata=(e)=>{
+
+const uimarker=()=>{
+    document.getElementById("tbody").innerHTML="";
+    for(let i=0; i<users.length;i++){
+        let tr=document.createAttribute("tr")
+        let td=document.createAttribute("td")
+        td.innerHTML=users[i]+1
+        let td1=document.createAttribute("td")
+        td1.innerHTML=users[i].name
+        let td2=document.createAttribute("td")
+        td2.innerHTML=users[i].category
+        let td3=document.createAttribute("td")
+        td3.innerHTML=users[i].price
+        let td4=document.createAttribute("td")
+        td4.innerHTML=users[i].description
+
+        tr.appendChild(td1,td2,td3,td3)
+        tbody.appendChild(tr)
+    }
+}
+
+const handaleData=(e)=>{
     e.preventdefault();
 
     let users={
@@ -8,5 +29,6 @@ const handaledata=(e)=>{
         price:document.getElementById("price").value,
         description:document.getElementById("description").value
     };
+    uimarker();
 }
-document.getElementById("form").addEventListener("submit",handaledata)
+document.getElementById("userData").addEventListener("submit",handaleData);
