bc06bce4b4a277493ce9bfe0c68ae643c797f5ef
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-08-21 15:58:08 +0530

    javascript

---
+++ lacture 21/index.js
@@ -0,0 +1,38 @@
+const dice = document.getElementById('dice');
+const rollbtn=document.getElementById('roll');
+
+let randomdice=()=>{
+    const random=Math.floor(Math.random()*10);
+    if(random>=1&&random<=6){
+        rolldice(random);
+    }
+    else{
+        randomdice();
+    }
+}
+const rolldice=random=>{
+    dice.style.animation='rolling 4s';
+    setTimeout(()=>{
+        switch(random){
+            case 1:dice.style.transform='rotateX(0deg) rotateY(0deg)';
+            break;
+            case 6:dice.style.transform='rotateX(180deg) rotateY(0deg)';
+            break;
+            case 6:dice.style.transform='rotateX(180deg) rotateY(0deg)';
+            break;
+            case 6:dice.style.transform='rotateX(180deg) rotateY(0deg)';
+            break;
+            case 2:dice.style.transform='rotateX(-90deg) rotateY(0deg)';
+            break;
+            case 5:dice.style.transform='rotateX(-90deg) rotateY(0deg)';
+            break;
+            case 3:dice.style.transform='rotateX(0deg) rotateY(90deg)';
+            break;
+            case 4:dice.style.transform='rotateX(0deg) rotateY(-90deg)';
+            break;
+
+            default:break;
+        }
+        dice.style.animation='none';
+    },4050);
+}
