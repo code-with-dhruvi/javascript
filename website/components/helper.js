2a1edaee95b0c19bef9ecd2f0fe8ae399462d61a
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-09-05 13:22:43 +0530

    javascript

--- website/components/helper.js
+++ website/components/helper.js
@@ -15,3 +15,19 @@ export const createTag=(tag,value)=>{
     }
     return tagName;
 };
+// const getValue = (className) => {
+//     return document.querySelector(className).value;
+// };
+
+// export default getValue;
+
+// export const createTag = (tag, value) => {
+//     let tagName = document.createElement(tag);
+//     if (tag == "img") {
+//         tagName.src = value;
+//     }
+//     else {
+//         tagName.innerHTML = value;
+//     }
+//     return tagName;
+// };
--- website/js/product.js
+++ website/js/product.js
@@ -1,4 +1,4 @@
-import { createTag } from "../components/helper.js";
+import getValue, { createTag } from "../components/helper.js";
 import Navbar from "../components/Navbar.js";
 
 document.getElementById("navbar").innerHTML=Navbar();
@@ -6,6 +6,7 @@ document.getElementById("navbar").innerHTML=Navbar();
 let products=JSON.parse(localStorage.getItem("products")) || [];
 
 const mapper=(data)=>{
+    document.getElementById("productlist").innerHTML = "";
     data.map((ele)=>{
         let img=createTag("img",ele.img)
         let price=createTag("p",ele.price)
@@ -14,6 +15,36 @@ const mapper=(data)=>{
         let div=document.createElement("div")
         div.append(img,title,price,category)
         document.getElementById("productlist").append(div)
-    })
-}
-mapper(products)
+    });
+};
+mapper(products);
+const handleSort = (orderby)=>{
+    if(orderby=="lth"){
+        let temp=products.sort((a,b)=>a.price-b.price);
+        mapper(temp);
+    }
+    else{
+        let temp=products.sort((a,b)=>b.price-a.price);
+        mapper(temp);
+    }
+};
+const handleCategory=(category)=>{
+    let temp=products.filter((ele)=>ele.category==category);
+    mapper(temp);
+};
+document.getElementById("lth").addEventListener("click",()=>handleSort("lth"));
+document.getElementById("htl").addEventListener("click",()=>handleSort("htl"));
+
+document.getElementById("men").addEventListener("click",()=>handleCategory("men"));
+document.getElementById("women").addEventListener("click",()=>handleCategory("women"));
+
+document.getElementById("electronics").addEventListener("click",()=>handleCategory("electronics"));
+
+const search=(e)=>{
+    e.preventDefault();
+
+    let searchValue=getValue("#search");
+    let temp=products.filter((ele)=>ele.title.tolowercase().includes(searchValue.tolowercase()));
+    mapper(temp);
+};
+document.getElementById("serching").addEventListener("submit",search);
