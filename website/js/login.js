30a91989a2c642c710072371649960402cd8ca05
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-09-10 14:32:27 +0530

    javascript

--- website/js/login.js
+++ website/js/login.js
@@ -24,38 +24,3 @@ const handleData=(e)=>{
     }
 };
 document.querySelector("#userData").addEventListener("submit",handleData);
-// import getValue from "../components/helper.js";
-// import Navbar from "../components/Navbar.js";
-
-// document.getElementById("navbar").innerHTML = Navbar();
-
-// let users = JSON.parse(localStorage.getItem("users")) || [];
-// const handleData = (e) => {
-//   e.preventDefault();
-
-//   let user = {
-//     email: getValue(".email"),
-//     password: getValue(".password"),
-//   };
-
-//   let isMatched = users.filter(
-//     (ele) => ele.email == user.email && ele.password == user.password
-//   );
-
-//   if (isMatched.length > 0) {
-//     alert("login success")
-     
-//     localStorage.setItem("username",isMatched[0].username)
-
-
-//     localStorage.setItem("isLogin", true)
-
-
-//     window.location.href = "/index.html"
-//   }
-//   else {
-//     alert("login failure")
-//   }
-// };
-
-// document.querySelector("#userData").addEventListener("submit", handleData);
