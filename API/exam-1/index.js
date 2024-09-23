ab2c7827c04da1210e0bb3e286f8ce7079b8f060
dhruvi pipaliya <dhruvipipaliya41@gmail.com>
2024-09-23 21:55:04 +0530

    javascript

--- API/exam-1/index.js
+++ API/exam-1/index.js
@@ -1,34 +1,38 @@
 console.log("starting");
 
-const getdata=async()=>{
-    let req=await fetch("https://dummyjson.com/products");
-    let res=await req.json();
-    console.log(res);
-    mapper(res.product)
+const getdata = async () => {
+  let req = await fetch("https://dummyjson.com/products");
+  let res = await req.json();
+  console.log(res);
+  mapper(res.products)
 }
+
 getdata();
-console.log("geting data");
+console.log("getting data");
 
-const mapper=(data)=>{
-    data.map((iteam)=>{
-        let container=document.createElement("div")
-        container.className="product";
-        let title=document.createElement("h1")
-        title.innerHTML=iteam.title
-        let description=document.createElement("p")
-        description.innerHTML=iteam.description
-        let category=document.createElement("h2")
-        category.innerHTML=iteam.category
-        let price=document.createElement("p")
-        price.innerHTML=iteam.price
-        let rating=document.createElement("h3")
-        rating.innerHTML=iteam.rating
-        let stock=document.createElement("p")
-        stock.innerHTML=iteam.stock
+const mapper = (data) => {
+  data.map((item) => {
+    let container = document.createElement("div")
+    container.className = "product";
+    let img = document.createElement("img");
+    img.src = item.thumbnail;
+    img.alt = item.title;
+    let title = document.createElement("h1")
+    title.innerHTML = item.title
+    let description = document.createElement("p")
+    description.innerHTML = item.description
+    let category = document.createElement("h2")
+    category.innerHTML = item.category
+    let price = document.createElement("p")
+    price.innerHTML = item.price
+    let rating = document.createElement("h3")
+    rating.innerHTML = item.rating
+    let stock = document.createElement("p")
+    stock.innerHTML = item.stock
 
-       
-        container.append(title,price,id,category,rating,stock)
-        document.getElementById("product-list").append(container);
-    })
+    container.append(img, title, description, price, category, rating, stock)
+    document.getElementById("product-list").append(container);
+  })
 }
-console.log("endding");
+
+console.log("ending");
